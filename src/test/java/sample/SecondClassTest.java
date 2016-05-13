5906d0c8be89db519c47f918a8c4718447310978
Belskii Artem <belskiiartem@gmail.com>
2016-05-13 17:05:34 +0300

    red test

--- src/test/java/sample/SecondClassTest.java
+++ src/test/java/sample/SecondClassTest.java
@@ -11,7 +11,7 @@ public class SecondClassTest {
 
 	@Test
 	public void testEchoSomething() {
-			assertEquals("sss", second.EchoSomething());	
+			assertEquals("failed", second.EchoSomething());	
 	};
 
 }
